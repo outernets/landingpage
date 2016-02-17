dc480de36579842cdc297fd591f332d7a15ce6b7
Amit Friedberg <amit@Amits-MacBook-Pro.local>
2016-02-17 17:21:16 -0500

    - using BoxGeometry we create a cube and use images as texture for its
    faces.
    - the camera perspective is set to be from inside the cube
    - when using the arrows keys the cube is rotating (left or right) using
    Tweens for animations

--- app/js/Application.js
+++ app/js/Application.js
@@ -3,31 +3,99 @@ var WIDTH = window.innerWidth
 
 export default class Application {
 
-  constructor (container) {
+  constructor (container, textures) {
+    var self = this;
     this.container = container;
 
-    this.camera = new THREE.PerspectiveCamera(60, WIDTH / HEIGHT, 1, 5000),
-    this.camera.position.set(0, 0, 500)
+    this.camera = new THREE.PerspectiveCamera(60, WIDTH / HEIGHT, 1, 5000);
+    //this.camera = new THREE.PerspectiveCamera(60, WIDTH / HEIGHT, 0, 0);
+    this.camera.position.set(0, 0, 180);
 
     this.renderer = new THREE.WebGLRenderer({
       alpha: true,
       antialias: true,
     })
-    this.renderer.setSize(WIDTH, HEIGHT)
-    this.container.appendChild(this.renderer.domElement)
+    this.renderer.setSize(WIDTH, HEIGHT);
+    this.container.appendChild(this.renderer.domElement);
 
     this.scene = new THREE.Scene();
 
     var light = new THREE.AmbientLight(0xff0000);
     this.scene.add(light);
 
-    var geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    var material = new THREE.MeshBasicMaterial({color: 0x000000});
 
-    this.mesh = new THREE.Mesh( geometry, material );
-    this.scene.add( this.mesh );
+    var materials = [];
+    for(var i = 0; i < 6; i++) {
+      materials.push(new THREE.MeshBasicMaterial({
+          map : textures[i],
+          side: THREE.DoubleSide,
+      }));
+    }
 
-    window.addEventListener('resize', this.onResize.bind(this))
+    var geometry = new THREE.BoxGeometry(WIDTH, WIDTH, WIDTH);
+
+
+    this.mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+    this.scene.add(self.mesh);
+
+    console.log('MESH = ', this.mesh);
+    console.log('GEOMETRY = ', geometry);
+
+    window.addEventListener('resize', this.onResize.bind(this));
+
+    //todo: after each rotation replace all face images but the one that is currently in front of the camera
+
+    // map for cube faces relative to initial face
+    //mesh.material.materials[0] -> right from facing front
+    //mesh.material.materials[1] -> left from facing front
+    //mesh.material.materials[2] -> top or bottom (not sure)
+    //mesh.material.materials[3] -> top or bottom (not sure)
+    //mesh.material.materials[4] -> facing front
+    //mesh.material.materials[5] -> opposite of facing front
+
+    // to change image on one face we can use this:
+    //self.mesh.material.materials[0] = new THREE.MeshBasicMaterial({
+    //  map : textures[textures.length - 1], // texture from some image
+    //  side: THREE.DoubleSide,
+    //});
+    document.onkeydown = function(e) {
+      switch (e.keyCode) {
+        case 37:
+          console.log('onkeydown: left');
+          createjs.Tween.get(self.mesh.rotation)
+            .wait(200)
+            .to({y: "-" + Math.PI/2}, 300)
+            .call(function(){
+
+            });
+          break;
+        case 38:
+          console.log('onkeydown: up');
+          createjs.Tween.get(self.mesh.rotation)
+            .wait(200)
+            .to({x: "-" + Math.PI/2}, 300)
+            .call(function(){});
+          break;
+        case 39:
+          console.log('onkeydown: right createjs = ', createjs);
+          createjs.Tween.get(self.mesh.rotation)
+            .wait(200)
+            .to({y: "+" + Math.PI/2}, 300)
+            .call(function(){
+
+            });
+          break;
+        case 40:
+          console.log('onkeydown: down');
+          createjs.Tween.get(self.mesh.rotation)
+            .wait(200)
+            .to({x: "+" + Math.PI/2}, 300)
+            .call(function(){
+
+            });
+          break;
+      }
+    };
   }
 
   onResize (e) {
@@ -40,8 +108,8 @@ export default class Application {
   }
 
   loop () {
-    this.mesh.rotation.x += 0.005;
-    this.mesh.rotation.y += 0.01;
+    //this.mesh.rotation.x += 0.005;
+    //this.mesh.rotation.y += 0.01;
 
     this.renderer.render(this.scene, this.camera)
     requestAnimationFrame(this.loop.bind(this))
--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,32 @@
 /* jshint devel:true */
 import Application from './Application'
 
-var app = new Application(document.body)
-app.loop()
+function loadTextures (callback) {
+  var loadedTextures = [];
+  var loaded = 0;
+  var textureLoader = new THREE.TextureLoader();
+  var textureURLs = ["/images/1.jpg", "/images/2.jpg", "/images/3.jpg", "/images/4.jpg", "/images/5.jpg", "/images/6.jpg", "/images/t1.jpg", "/images/t2.jpg"];
+  for(var i = 0; i < textureURLs.length; i++) {
+    textureLoader.load(textureURLs[i], function(t) {
+      loadedTextures.push(t);
+      loaded++;
+      if(loaded === textureURLs.length){
+        var app = new Application(document.body, loadedTextures);
+        app.loop()
+      }
+    });
+  }
+
+  //textureLoader.addEventListener('load', function (event) {
+  //  loadedTextures.push(event.content);
+  //  loaded++;
+  //  if(loaded === textureURLs.length){
+  //    var app = new Application(document.body, loadedTextures)
+  //    app.loop()
+  //  }
+  //});
+
+
+}
+
+loadTextures();
